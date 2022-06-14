8edf14ce60e6dea1fbce459793995bf2f8a9ccba
moaitodev <bymavc@outlook.com>
2022-06-14 15:11:31 -0500

    💄 Included pagination

--- lib/commits.ts
+++ lib/commits.ts
@@ -11,11 +11,15 @@ export type ListCommitsResponseType = GetResponseTypeFromEndpointMethod<
   typeof octokit.rest.repos.listCommits
 >
 
-const getCommits = async (): Promise<ListCommitsResponseType | unknown> => {
+const getCommits = async (
+  page: number
+): Promise<ListCommitsResponseType | unknown> => {
   try {
     const response = await octokit.rest.repos.listCommits({
       owner: config.owner,
       repo: config.repo,
+      page: page,
+      per_page: 5,
     })
     return response
   } catch (err: unknown) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,59 @@
-import type { NextPageContext } from "next"
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Head from "next/head"
-import React from "react"
+import React, { useState } from "react"
 import Commit from "../components/Commit"
 import Header from "../components/Header"
 import { getCommits, ListCommitsResponseType } from "../lib/commits"
 import styles from "../styles/Home.module.css"
 
+const initialPage = 1
+
 const Home: React.FC<{ commits: ListCommitsResponseType }> = ({ commits }) => {
-  const { data } = commits
+  const [page, setPage] = useState(initialPage)
+  const [projectCommits, setProjectCommits] = useState(commits.data)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleLoadMore = async () => {
+    setIsLoading(true)
+    const newPage = page + 1
+    const moreCommits = (await getCommits(newPage)) as ListCommitsResponseType
+    if (moreCommits.data) {
+      setProjectCommits((projectCommits) => [
+        ...projectCommits,
+        ...moreCommits.data,
+      ])
+      setPage(newPage)
+    }
+    setIsLoading(false)
+  }
 
   let content
-  if (data) {
+  if (projectCommits) {
     content = (
       <>
         <h1 className={styles.Title}>Latest Commits</h1>
         <div>
-          {data.map((commit, index) => (
+          {projectCommits.map((commit, index) => (
             <Commit key={index} commit={commit} />
           ))}
+        </div>
+        <div className={styles.ButtonContainer}>
+          <button
+            onClick={handleLoadMore}
+            className={styles.Button}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <FontAwesomeIcon
+                icon={faSpinner}
+                pulse
+                className={styles.Spinner}
+              />
+            ) : (
+              "Load Older Commits"
+            )}
+          </button>
         </div>
       </>
     )
@@ -47,7 +83,7 @@ const Home: React.FC<{ commits: ListCommitsResponseType }> = ({ commits }) => {
 }
 
 export async function getServerSideProps() {
-  const commits = await getCommits()
+  const commits = await getCommits(initialPage)
 
   return {
     props: {
