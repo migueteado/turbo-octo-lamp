b85afcc88e6d1243807479dd8ab7c66d6ee494ae
moaitodev <bymavc@outlook.com>
2022-06-14 08:37:00 -0500

    ✨ Commit component initial layout

---
+++ components/Commit.tsx
@@ -0,0 +1,41 @@
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Image from "next/image"
+import { ListCommitsResponseType } from "../lib/commits"
+import styles from "./Commit.module.css"
+
+interface CommitProps {
+  commit: ListCommitsResponseType["data"][0]
+}
+
+const Commit = ({ commit }: CommitProps) => {
+  console.log(commit)
+  return (
+    <div className={styles.Commit}>
+      <a href={commit.author?.html_url} target="_blank" rel="noreferrer">
+        <div className={styles.Author}>
+          <div className={styles.AuthorImageContainer}>
+            <Image
+              layout="responsive"
+              alt="profile image"
+              src={`${commit.author?.avatar_url}`}
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={styles.AuthorName}>{commit.author?.login}</div>
+        </div>
+      </a>
+      <div className={styles.CommitInfo}>
+        <div className={styles.GithubLink}>
+          <a href={commit.html_url} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faUpRightFromSquare} />
+          </a>
+        </div>
+        <div className={styles.Message}>{commit.commit.message}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Commit
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from "next"
+import type { NextPageContext } from "next"
 import Head from "next/head"
-import Image from "next/image"
+import React from "react"
+import Commit from "../components/Commit"
 import Header from "../components/Header"
+import { getCommits, ListCommitsResponseType } from "../lib/commits"
+import styles from "../styles/Home.module.css"
 
-const Home: NextPage = () => {
+const Home: React.FC<{ commits: ListCommitsResponseType }> = ({ commits }) => {
+  const { data } = commits
+
   return (
     <div>
       <Head>
@@ -12,9 +17,26 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main></main>
+      <main className={styles.MainContent}>
+        <h1 className={styles.Title}>Latest Commits</h1>
+        <div>
+          {data.map((commit, index) => (
+            <Commit key={index} commit={commit} />
+          ))}
+        </div>
+      </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const commits = await getCommits()
+
+  return {
+    props: {
+      commits,
+    },
+  }
 }
 
 export default Home
